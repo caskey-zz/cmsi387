a8428394d6d32e01afa4af9c92c95c32f9311947
Caskey Dickson <caskey@technocage.com>
2015-01-18 18:48:31 -0800

    makefile and kernel stack

--- hw2/loader.s
+++ hw2/loader.s
@@ -4,6 +4,7 @@ FL_ALIGN equ 1 << 0
 FL_MEMINFO equ 1 << 1
 FLAGS equ FL_ALIGN | FL_MEMINFO
 CHECKSUM equ -(MAGIC_NUMBER + FLAGS)
+KERNEL_STACK_SIZE equ 4096
 
 section .text
 align 4
@@ -15,4 +16,10 @@ loader:
   mov eax, 0xCAFEBABE
 
 .loop:
+  mov esp, kernel_stack + KERNEL_STACK_SIZE
   jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+  resb KERNEL_STACK_SIZE
